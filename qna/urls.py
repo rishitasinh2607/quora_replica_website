02db48fbb3add7e48f63ac7c9cad55a3aebad673
rishita singh <57592021+rishitasinh2607@users.noreply.github.com>
2020-06-05 11:03:57 +0530

    first commit

---
+++ qna/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from qna.views import index,signup,PostQuestion,QuestionDetail,add_answer_to_question,logout_view
+
+app_name = 'qna'
+
+urlpatterns = [
+    path('',index,name = 'index'),
+    path('signup/',signup,name = 'signup'),
+    path('qpost/',PostQuestion.as_view(),name = 'post'),
+    path('detail/<int:pk>/',QuestionDetail.as_view(), name = 'detail'),
+    path('detail/<int:pk>/answer/',add_answer_to_question,name = 'answer'),
+    path('logout/',logout_view,name = 'logout'),
+    ]
